a39a8510eabbeed7e40cf2c601cba4ff4da7a54a
emanoeldelfino <emanoeldelfno@gmail.com>
2021-03-06 13:48:20 -0300

    Update compare_rows.py

--- 13-working_with_excel/ideas/compare_rows.py
+++ 13-working_with_excel/ideas/compare_rows.py
@@ -1,10 +1,27 @@
 import openpyxl
+import pprint
+import string
+import sys
 
-wb = openpyxl.load_workbook('sample_data.xlsx')
-sheet = wb['Sheet1']
+args = sys.argv[1:]
+
+alphabet = string.ascii_uppercase
+
+wb = openpyxl.load_workbook(args[0])
+sheet = wb.active # wb['Sheet1']
+
+table = [[] for _ in range(sheet.max_row)]
 
 for row in range(1, sheet.max_row + 1):
     for column in range(sheet.max_column):
-        print(sheet[row][column].value)
+        table[row - 1].append(sheet[row][column].value)
 
-# print(sheet[1][0])
+for column_letter in alphabet[:len(table[0])]:
+    print(f'{column_letter:^11}', end='')
+print()
+
+for row in range(sheet.max_row):
+    print(row, end=' ')
+    for column in range(len(table[row])):
+        print(f'{table[row][column]:<11}', end='')
+    print()
